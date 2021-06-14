d71cf0e2605a73f3868e364876cb4858561edf18
Amber-Russell <83440345+Amber-Russell@users.noreply.github.com>
2021-06-14 12:48:56 -0700

    relocated

---
+++ Projects1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+window.alert ("Hi there!");
+document.alert("Ready or not! Here I come!");
+
+var A= 'this is a string';
+
+document.write(A);
+
+window.alert ("The saying goes\"There are plenty of fish in the sea\" and we believe that to be true. If it "+
+"was false, we would have been alone.")
+
+var B="plenty" + "fish"
+document.write(B)
+
+var family= "Robinsons", Mom="Sue", Dad = "Drew", Son = "Charlie";
+document.write (Son);
+
+var family= family.fontcolor("green");
+document.write(family);
+
+var A = A.fontcolor("red");
+document.write(A)
