b93f388bdf62aa0e5e9de57db9c802c6a89c4c35
Amber-Russell <83440345+Amber-Russell@users.noreply.github.com>
2021-06-15 14:21:48 -0700

    math

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -2,3 +2,19 @@ function addition_Function() {
     var addition= 2 + 2;
     document.getElementById("Math").innerHTML="2+2="+ addition;
 }
+function subtract_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML ="5-2="+ subtraction;
+}
+function multiply() {
+    var simple_Math =6 * 8;
+    document.getElementById("Math").innerHTML ="6 x 8=" + simple_Math;
+}
+function division() {
+    var simple_Math= 48 / 6;
+    document.getElementById("Math").innerHTML = "48 / 6 =" + simple_Math;
+}
+function more_Math() {
+    var simple_Math = (1 +2) * 10 / 2-5;
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, didvided in half and then subtracted by 5 equals" + simple_Math;
+}
