c4ed62a6b07d5bfde3e37d46292eeb7fa13776f1
Amber-Russell <83440345+Amber-Russell@users.noreply.github.com>
2021-06-14 17:50:32 -0700

    created project functions and added comments

---
+++ Project2_Functions/JS/main.js
@@ -0,0 +1,6 @@
+function myFunction() {     //tells program to run ID myFunction 
+    var sentence = "I am learning";   //first part or var sentence
+    sentence +=" a lot from this book!";   //tells program to continue sentence 
+    document.getElementById("concatenate").innerHTML = sentence;  //tells program to find element Id 
+    //inside of HTML file by ID concatenate and display sentence.
+}
--- Projects1_expressions_alert/JS/main.js
+++ Projects1_expressions_alert/JS/main.js
@@ -1,27 +1,29 @@
 window.alert ("Hi there!");  //pop up window with "hi there"
 window.alert("Ready or not! Here I come!");   //pop up window statment "ready or not"
 
-var A= 'this is a string'; defines variable "A" a string
+var A= ' this is a string';    //defines variable "A" a string
+
+var A = A.fontcolor("red");
+document.write(A)
 
 document.write(A);   //announces "A" value
 
 window.alert ("The saying goes\"There are plenty of fish in the sea\" and we believe that to be true. If it "+
 " was false, we would have been alone.")    //used '\' to separate the quotations so the program doesnt think it is a function
 
-var B="plenty" + "fish"   //added two variables and created id
+var B=" plenty" + "fish"   //added two variables and created id
 document.write(B)
 
-var family= "Robinsons", Mom="Sue", Dad = "Drew", Son = "Charlie";
+var family= " Robinsons", Mom=" Sue", Dad = " Drew", Son = " Charlie";
 document.write (Son);   //assigned id to family element
 
 var family= family.fontcolor("green");   //assigned family element font color
 document.write(family);     //processed funtion and returned results
 
-var A = A.fontcolor("red");
-document.write(A)
 
 function My_First_Function() {     //Defining a function and naming it
     var str="This text is green!";   //defied variable and gave it string value
     var result = str.fontcolor("green");   // used the fontcolor method on str var
     document.getElementById("Green_Text").innerHTML = result;  //put the value result into HTML element with "green text" id
 }
+
